6434f92e4efe43c0fc2b469eb69026ada19911ef
Tiago Junior <tiagojr159@hotmail.com>
2025-04-28 17:19:31 -0300

    login

--- api_php/login.php
+++ api_php/login.php
@@ -1,7 +1,7 @@
 <?php
 header('Access-Control-Allow-Origin: *');
-header('Access-Control-Allow-Methods: POST');
-header('Content-Type: application/json');
+header('Access-Control-Allow-Methods: POST, GET, OPTIONS');
+header('Access-Control-Allow-Headers: Content-Type, Authorization');
 
 include 'conexao.php';
 
