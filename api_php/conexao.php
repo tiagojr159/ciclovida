d6d6da35ff6ff3b6ed756ef1be0616d8a1367b87
Tiago Junior <tiagojr159@hotmail.com>
2025-04-28 22:20:20 -0300

    login  verificar session

--- api_php/conexao.php
+++ api_php/conexao.php
@@ -5,12 +5,12 @@ $user = 'root';
 $pass = '';
 
 
-
+/*
 $host = 'localhost'; // servidor
 $db = 'ki6com20_ciclovida'; // usuario do banco
 $pass = 'igarassu12!@'; // senha do usuario do banco
 $user = 'ki6com20_paixao'; // nome do banco
-
+*/
 
 try {
     $pdo = new PDO("mysql:host=$host;dbname=$db;charset=utf8", $user, $pass);
--- api_php/login.php
+++ api_php/login.php
@@ -1,6 +1,16 @@
 <?php
+if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+    header('Access-Control-Allow-Origin: *');
+    header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
+    header('Access-Control-Allow-Headers: Content-Type, Authorization');
+    http_response_code(204);
+    exit;
+}
+
+// Continua seu código normal depois
 header('Access-Control-Allow-Origin: *');
-header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE');
+header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
+header('Access-Control-Allow-Headers: Content-Type, Authorization');
 header('Content-Type: application/json');
 
 include 'conexao.php';
