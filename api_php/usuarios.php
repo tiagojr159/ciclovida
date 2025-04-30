a80a838ede8280894bd81cdb908158c6d824b5c6
Tiago Junior <tiagojr159@hotmail.com>
2025-04-30 01:18:15 -0300

    ajuste no cors

--- api_php/usuarios.php
+++ api_php/usuarios.php
@@ -1,7 +1,17 @@
 <?php
-header('Access-Control-Allow-Origin: *');
-header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE');
-header('Content-Type: application/json');
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    header("Access-Control-Allow-Origin: *");
+    header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
+    header("Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With");
+    http_response_code(200);
+    exit();
+}
+
+// Cabeçalhos para todas as requisições
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With");
+header("Content-Type: application/json");
 
 $metodo = $_SERVER['REQUEST_METHOD'];
 
